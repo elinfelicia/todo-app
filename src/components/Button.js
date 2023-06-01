fee4f312d0803b8535f69e1c49899d1a41590ac6
Elin <elinriihimaki@gmail.com>
2023-06-01 12:23:35 +0200

    Button and Form

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,24 @@
 import PropTypes from "prop-types";
 import { BsPlus } from "react-icons/bs";
 
-const Button = () =>{
+const Button = ({children, type, isDisabled}) =>{
     return (
-        <button>Button</button>
+        <button type={type} disabled={isDisabled} className="btn">
+            <BsPlus fontSize="52px" />
+            {children}
+        </button>
     )
 };
+
+Button.defaultProps = {
+    type: "button",
+    isDisabled: false,
+}
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    type: PropTypes.string,
+    isDisabled: PropTypes.bool,
+}
 
 export default Button;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import List from "./List";
 import Button from "./Button";
+import Form from "./Form";
 import todoData from "../data/Todo";
 
 const Home =() => {
     const [todo, setTodo] = useState(todoData);
     return (
-        <div>
+        <div className="container">
+            <Form />
             <List todo={todo} />
         </div>
     )
